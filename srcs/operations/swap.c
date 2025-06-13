b03201b7cb701a637e6b124570187dd43900a738
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-06-13 13:49:07 +0100

    Create swap operation functions

--- srcs/operations/swap.c
+++ srcs/operations/swap.c
@@ -3,37 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: g24force <g24force@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gjose-fr <gjose-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 11:37:46 by g24force          #+#    #+#             */
-/*   Updated: 2025/06/10 18:24:26 by g24force         ###   ########.fr       */
+/*   Updated: 2025/06/13 13:01:08 by gjose-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+
+// Swaps the first 2 elements of stack a 
 void	sa(t_stack *stack)
 {
-	int	temp;
+	t_node	*first;
+	t_node	*second;
 
 	if (stack->count < 2)
 		return ;
-	temp = stack->head->next->value;
-	stack->head->next->value = stack->head->value;
-	stack->head->value = temp;
+	first = stack->head;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	stack->head = second;
 }
 
+// Swaps the first 2 elements of stack b
 void	sb(t_stack *stack)
 {
-	int	temp;
+	t_node	*first;
+	t_node	*second;
 
 	if (stack->count < 2)
 		return ;
-	temp = stack->head->next->value;
-	stack->head->next->value = stack->head->value;
-	stack->head->value = temp;
+	first = stack->head;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	stack->head = second;
 }
 
+// Swaps the first 2 elements of both stacks at the same time
 void	ss(t_stack *stack_a, t_stack *stack_b)
 {
 	sa(stack_a);
