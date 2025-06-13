b3c33fa1a972e6e55befabbd76c998dd5fdab0bd
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-06-13 13:49:20 +0100

    Create push operation functions

--- srcs/operations/push.c
+++ srcs/operations/push.c
@@ -3,36 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   push.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: g24force <g24force@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gjose-fr <gjose-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 11:53:32 by g24force          #+#    #+#             */
-/*   Updated: 2025/06/10 18:34:44 by g24force         ###   ########.fr       */
+/*   Updated: 2025/06/13 13:02:47 by gjose-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-// move function somewhere else
+// TODO: move function somewhere else
 void	stack_add_top(t_stack *stack, t_node *new)
 {
 	new->next = stack->head;
 	stack->head = new;
 }
 
-void	pa(t_stack *stack_b, t_stack *stack_a)
+// Takes the first element of stack b and puts it on top of stack a
+void	pa(t_stack *stack_a, t_stack *stack_b)
 {
-	int	temp;
+	t_node	*temp;
 
 	if (stack_b->count < 1)
 		return ;
-	stack_add_top(stack_a, stack_b->head);
-	stack_a->count++;
+	temp = stack_b->head;
 	stack_b->head = stack_b->head->next;
 	stack_b->count--;
+	stack_add_top(stack_a, temp);
+	stack_a->count++;
 }
 
-void	ss(t_stack *stack_a, t_stack *stack_b)
+// Takes the first element of stack a and puts it on top of stack b
+void	pb(t_stack *stack_a, t_stack *stack_b)
 {
-	sa(stack_a);
-	sb(stack_b);
+	t_node	*temp;
+
+	if (stack_a->count < 1)
+		return ;
+	temp = stack_a->head;
+	stack_a->head = stack_a->head->next;
+	stack_a->count--;
+	stack_add_top(stack_b, temp);
+	stack_b->count++;
 }
