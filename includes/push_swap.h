526753b23d4a650e33940e6ff056f395246c2185
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-06-13 13:50:00 +0100

    Add new functions to header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: g24force <g24force@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gjose-fr <gjose-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 11:06:36 by g24force          #+#    #+#             */
-/*   Updated: 2025/06/10 18:01:49 by g24force         ###   ########.fr       */
+/*   Updated: 2025/06/13 13:38:53 by gjose-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+
+// TODO: DELETE NAO TE ESQUECAS
+# include <stdio.h> 
 
 typedef struct s_node
 {
@@ -25,6 +28,26 @@ typedef struct s_stack
 {
 	int		count;
 	t_node	*head;
+	t_node	*tail;
 }	t_stack;
+
+// del.c TODO: DELETE NAO TE ESQUECAS
+void	print_stack(t_stack *stack);
+void	print_stacks(t_stack *stack_a, t_stack *stack_b);
+
+
+// 		***OPERATIONS***
+
+// push.c
+void	pa(t_stack *stack_a, t_stack *stack_b);
+void	pb(t_stack *stack_a, t_stack *stack_b);
+
+// rotate.c
+void	ra(t_stack *stack);
+
+// swap.c
+void	sa(t_stack *stack);
+void	sb(t_stack *stack);
+void	ss(t_stack *stack_a, t_stack *stack_b);
 
 #endif
