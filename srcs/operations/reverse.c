afc7707e205cb73f3a50d54582161bae93ac4fe2
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-06-16 13:35:20 +0100

    Correct functions comments (*down)

--- srcs/operations/reverse.c
+++ srcs/operations/reverse.c
@@ -6,13 +6,13 @@
 /*   By: gjose-fr <gjose-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 12:09:15 by gjose-fr          #+#    #+#             */
-/*   Updated: 2025/06/16 13:26:23 by gjose-fr         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:34:48 by gjose-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-// Shifts all the elements of the stack a up by one position.
+// Shifts all the elements of the stack a down by one position.
 void	rra(t_stack *stack)
 {
 	t_node	*second_to_last;
@@ -28,7 +28,7 @@ void	rra(t_stack *stack)
 	stack->tail = second_to_last;
 }
 
-// Shifts all the elements of the stack b up by one position.
+// Shifts all the elements of the stack b down by one position.
 void	rrb(t_stack *stack)
 {
 	t_node	*second_to_last;
@@ -44,7 +44,7 @@ void	rrb(t_stack *stack)
 	stack->tail = second_to_last;
 }
 
-// Shifts all the elements of both stacks up by one position at the same time.
+// Shifts all the elements of both stacks down by one position at the same time.
 void	rrr(t_stack *stack_a, t_stack *stack_b)
 {
 	rra(stack_a);
